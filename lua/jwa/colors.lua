f85b980351be9349ceb47364579a78a44abdb95d
hwangfucius <junzhe.hwangfu@gmail.com>
2026-08-21 23:55:12 +0200

    style: markdown uses default theme now

--- lua/jwa/colors.lua
+++ lua/jwa/colors.lua
@@ -33,93 +33,92 @@ local M = {}
 
 -- require("jwa.colors").setup()
 function M.setup()
-	-- (a) Web / markup files -> 256_noir + diagnostic palette tweaks.
-	vim.api.nvim_create_autocmd("FileType", {
-		pattern = {
-			"html",
-			"htmlangular",
-			"css",
-			"scss",
-		},
-		callback = function()
-			vim.cmd.colorscheme("256_noir")
-			vim.cmd.highlight("SignColumn guibg=#000000")
-			vim.cmd.highlight("DiagnosticHint guifg=grey")
-			vim.cmd.highlight("DiagnosticError guifg=red")
-			vim.cmd.highlight("DiagnosticSignWarn guifg=grey")
-			vim.cmd.highlight("DiagnosticSignError guifg=red")
-			vim.cmd.highlight("DiagnosticUnderlineError guisp=red")
-		end,
-	})
+    -- (a) Web / markup files -> 256_noir + diagnostic palette tweaks.
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+            "html",
+            "htmlangular",
+            "css",
+            "scss",
+        },
+        callback = function()
+            vim.cmd.colorscheme("256_noir")
+            vim.cmd.highlight("SignColumn guibg=#000000")
+            vim.cmd.highlight("DiagnosticHint guifg=grey")
+            vim.cmd.highlight("DiagnosticError guifg=red")
+            vim.cmd.highlight("DiagnosticSignWarn guifg=grey")
+            vim.cmd.highlight("DiagnosticSignError guifg=red")
+            vim.cmd.highlight("DiagnosticUnderlineError guisp=red")
+        end,
+    })
 
-	-- (b) Programming languages, prose, structured config -> dracula.
-	-- The pattern list spans nearly every language we touch; anything missing
-	-- here falls through to whatever colorscheme was last set (typically the
-	-- one set globally on startup, or the (a) theme if the previous buffer
-	-- was web/markup).
-	vim.api.nvim_create_autocmd("FileType", {
-		pattern = {
-			"lisp",
-			"scheme",
-			"clojure",
-			"fennel", -- joined (b) 2026-08-14 alongside the fennel-ls wiring
-			"haskell",
-			"lua",
-			"go",
-			"rust",
-			"pascal",
-			"py",
-			"python",
-			"javascript",
-			"typescript",
-			"sh",
-			"bash",
-			"c",
-			"cpp",
-			"proto",
-			"zig",
-			"perl",
-			"dune",
-			"ocaml",
-			"elixir",
-			"erlang",
-			"toml",
-			"yml",
-			"yaml",
-			"json",
-			"jsonc",
-			"vim",
-			"make",
+    -- (b) Programming languages, prose, structured config -> dracula.
+    -- The pattern list spans nearly every language we touch; anything missing
+    -- here falls through to whatever colorscheme was last set (typically the
+    -- one set globally on startup, or the (a) theme if the previous buffer
+    -- was web/markup).
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+            "lisp",
+            "scheme",
+            "clojure",
+            "fennel", -- joined (b) 2026-08-14 alongside the fennel-ls wiring
+            "haskell",
+            "lua",
+            "go",
+            "rust",
+            "pascal",
+            "py",
+            "python",
+            "javascript",
+            "typescript",
+            "sh",
+            "bash",
+            "c",
+            "cpp",
+            "proto",
+            "zig",
+            "perl",
+            "dune",
+            "ocaml",
+            "elixir",
+            "erlang",
+            "toml",
+            "yml",
+            "yaml",
+            "json",
+            "jsonc",
+            "vim",
+            "make",
 
-			"json",
-			"yml",
-			"yaml",
+            "json",
+            "yml",
+            "yaml",
 
-			"fortran",
-			"vhdl",
+            "fortran",
+            "vhdl",
 
-			"text",
-			"markdown", -- joined (b) 2026-08; had its own green-theme group before
+            "text",
 
-			"oil",
+            "oil",
 
-			"cabal",
+            "cabal",
 
-			"gitignore",
-			"gitcommit",
-		},
-		callback = function()
-			-- NOTE: `:colorscheme X` runs `:highlight clear` and redefines
-			-- everything from scratch. If you want highlight overrides,
-			-- set them AFTER this line.
-			vim.cmd.colorscheme("dracula")
-			-- vim.cmd.highlight("Normal guibg=#32324e")
-			vim.cmd.highlight("Normal guibg=#022800")
-			vim.cmd.highlight("LspInlayHint guibg=#022800 guifg=#6272a4")
-			vim.cmd.highlight("@keyword guifg=#0078cd")
-			vim.cmd.highlight("@type.builtin gui=none")
-		end,
-	})
+            "gitignore",
+            "gitcommit",
+        },
+        callback = function()
+            -- NOTE: `:colorscheme X` runs `:highlight clear` and redefines
+            -- everything from scratch. If you want highlight overrides,
+            -- set them AFTER this line.
+            vim.cmd.colorscheme("dracula")
+            -- vim.cmd.highlight("Normal guibg=#32324e")
+            vim.cmd.highlight("Normal guibg=#022800")
+            vim.cmd.highlight("LspInlayHint guibg=#022800 guifg=#6272a4")
+            vim.cmd.highlight("@keyword guifg=#0078cd")
+            vim.cmd.highlight("@type.builtin gui=none")
+        end,
+    })
 end
 
 return M
